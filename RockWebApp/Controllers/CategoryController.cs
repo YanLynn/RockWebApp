d51fb84372d4deee2d133c79688bed07a3452857
Yan Lynn Aung <yan.lynn1991@gmail.com>
2023-10-13 11:13:28 +0800

    update

---
+++ RockWebApp/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RockWebApp.Controllers.Data;
+using RockWebApp.Models;
+
+namespace RockWebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) { 
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategory = _db.Categories.ToList();
+            return View(objCategory);
+        }
+
+        public IActionResult Create() {
+
+            return View();
+
+        }
+    }
+}
---
+++ RockWebApp/Controllers/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RockWebApp.Models;
+
+namespace RockWebApp.Controllers.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "John", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Cherry", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Snow", DisplayOrder = 4 }
+                );
+            base.OnModelCreating(modelBuilder);
+        }
+
+        
+    }
+}
