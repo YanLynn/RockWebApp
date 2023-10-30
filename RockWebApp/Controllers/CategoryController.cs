c6dfa7778732c071bfb4e4b7566328646759c29e
Yan Lynn Aung <yan.lynn1991@gmail.com>
2023-10-30 10:48:56 +0800

    update code

--- RockWebApp/Controllers/CategoryController.cs
+++ RockWebApp/Controllers/CategoryController.cs
@@ -20,7 +20,87 @@ namespace RockWebApp.Controllers
         public IActionResult Create() {
 
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString()){
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name");
+            }
+            if(ModelState.IsValid) {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created Successfully.";
+                //you can redirect another controller.... ("action","controller")
+                return RedirectToAction("Index");
+            }
+            return View();
+            
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.Find(id);
+            //Category? category1 = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //Category? category2 = _db.Categories.Where(c => c.Id == id).FirstOrDefault();
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["update"] = "Category Updated Successfully.";
+                //you can redirect another controller.... ("action","controller")
+                return RedirectToAction("Index");
+            }
+            return View();
 
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["delete"] = "Category Deleted Successfully.";
+            //you can redirect another controller.... ("action","controller")
+            return RedirectToAction("Index");
+          
+
+        }
+
     }
 }
--- RockWebApp/Models/Category.cs
+++ RockWebApp/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace RockWebApp.Models
 {
@@ -7,8 +8,12 @@ namespace RockWebApp.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(50)]
         public string Name { get; set; }
         [Required]
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage = "Display Order must be between 1-100.")]
         public int DisplayOrder { get; set; }
     }
 }
