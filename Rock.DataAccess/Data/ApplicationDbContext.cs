9555d25bb0878c8e5bed6de30ae93ff9f475a6d6
Yan Lynn Aung <yan.lynn1991@gmail.com>
2023-12-05 10:53:20 +0800

    extend identity user

--- Rock.DataAccess/Data/ApplicationDbContext.cs
+++ Rock.DataAccess/Data/ApplicationDbContext.cs
@@ -15,7 +15,7 @@ namespace Rock.DataAccess.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
-
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
