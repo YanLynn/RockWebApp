4d78a87a8710abd1abcd16d8ffc17b982413af1b
Yan Lynn Aung <yan.lynn1991@gmail.com>
2023-12-21 11:30:51 +0800

    add Shopping Cart Model and repository and applicationUser repository

---
+++ Rock.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using Rock.DataAccess.Data;
+using Rock.DataAccess.Repository.IRepository;
+using Rock.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db) {
+            _db = db;
+        }
+       
+    }
+}
---
+++ Rock.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,27 @@
+﻿using Rock.DataAccess.Data;
+using Rock.DataAccess.Repository.IRepository;
+using Rock.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db) {
+            _db = db;
+        }
+       
+        public void Update(ShoppingCart obj)
+        {
+            _db.ShoppingCarts.Update(obj);
+           
+        }
+    }
+}
